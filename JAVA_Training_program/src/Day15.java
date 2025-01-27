23831f382b65d7bd4cc5102f9575142dbbdd3d23
A-R-K-7 <136780092+A-R-K-7@users.noreply.github.com>
2025-01-27 19:33:17 +0530

    Day 15 commit

---
+++ JAVA_Training_program/src/Day15.java
@@ -0,0 +1,37 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+//[7,10,14,19,20]
+
+// floor of 15 is 14
+
+public class Day15{
+
+}
+
+class Solution4 {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i =0;i<nums.length;i++){            
+            if(hs.contains(nums[i])){
+                return true;
+            }
+            hs.add(nums[i]);
+        }
+        return false;
+    }
+}
+
+class Solution5 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            if(hm.containsKey(nums[i]) && i - hm.get(nums[i])<=k){
+                return true;
+            }
+            hm.put(nums[i],i);
+        }
+        return false;
+    }
+}
