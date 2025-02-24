045c208720ae97a8fc30104a3add38b4ea01e198
A-R-K-7 <136780092+A-R-K-7@users.noreply.github.com>
2025-02-24 12:59:38 +0530

    Feb 24 Commit

---
+++ JAVA_Training_program/src/Feb24.java
@@ -0,0 +1,24 @@
+public class Feb24 {
+    class Solution {
+        public boolean isPalindrome(String s) {
+            s = s.toLowerCase();
+            int len = s.length();
+            int left = 0;
+            int right = len-1;
+            while(left<=right){
+                while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                    left++;
+                }
+                while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                    right--;
+                }
+                if(s.charAt(left)!=s.charAt(right)){
+                    return false;        
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
